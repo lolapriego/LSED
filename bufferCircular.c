594bfde8993af6c6cc72975ebae4760a17b6d8eb
Lola Priego <lola@emtrics.com>
2012-05-08 19:51:59 +0300

    Ahora el bufferCircular se rellena con la salida y no con la entrada!

--- bufferCircular.c
+++ bufferCircular.c
@@ -6,16 +6,30 @@
  // =============
   // lee un dato del ADC lo almacena en un buffer, lo suma y lo devuelve
   // =============
- INLINE int bufferCircular (int tension, int * puntero_buffer, int * buffer) {
+  int bufferCircular (int tension, int *buffer) {
+    static int *puntero_buffer = 0;
+    static int flag = 0; // marca cuando empezamos a sacar del buffer
+    int muestra = 0;
+    
+    if ( !puntero_buffer)
+      puntero_buffer = buffer;
 
     if( puntero_buffer < buffer + retardo_reverberacion){
-      * puntero_buffer = tension / atenuacion_reverberacion;
-      puntero_buffer ++;
-    }
-    else{
-      puntero_buffer = buffer;
+      if( flag )
+        muestra = *puntero_buffer;
+     
       *puntero_buffer = tension / atenuacion_reverberacion;
+      puntero_buffer++;
     }
 
-    return buffer[retardo_reverberacion -1];
+    else{
+      flag = 1;
+      puntero_buffer = buffer;
+      muestra = *puntero_buffer;
+      *puntero_buffer = tension / atenuacion_reverberacion;
+      puntero_buffer++;
+    }
+
+     return muestra;
+
   }
