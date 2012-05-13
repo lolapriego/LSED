db6be2fd5ed9bd9ae2f8d8f2871a897d78d813ca
Lola Priego <lola@emtrics.com>
2012-05-13 22:23:43 +0200

    Añadida la función que nos permite gestionar los pre-sets de ecualización

--- gestionAvanzada.c
+++ gestionAvanzada.c
@@ -41,7 +41,7 @@
 
     printf("\n=========\nSeleccione alguno de los siguientes parametros de reverberacion a aplicar:\n");
     for (i=0, i< parametros; i++)
-      printf("%d) Atenuacion: %d   Reverberacion: %d\n");
+      printf("%d) Atenuacion: %d   Reverberacion: %d\n", i + 1, atenuaciones[i], retardos[i]);
     printf("O pulse E para añadir un parametro de reverberacion y aplicarlo\n");
 
     opcion = teclado() - '0';
@@ -65,4 +65,42 @@
       parametros ++;
     }
 
+  }
+
+  void GestionAvanzadaEcualizacion(){
+    static int ecualizaciones[9][7] = {{},{},{},{},{},{},{},{},{}}; //TODO crear pre-sets de ecualizacion con sentido
+    static int parametros = 3;
+
+    int i;
+    int opcion;
+
+    printf("\n=========\nSeleccione alguno de los siguientes parametros de ecualizacion a aplicar:\n");
+
+    for (i=0, i< parametros; i++){
+      printf("\n====BANDA:     32Hz   64Hz   125Hz   250Hz   500Hz   1kHz   2kHz=====\n");
+      printf("%d)Nivel      %d      %d      %d       %d       %d       %d       %d\n", i + 1, ecualizaciones[i][0], ecualizaciones[i][1], ecualizaciones[i][2]
+      ecualizaciones[i][3], ecualizaciones[i][4], ecualizaciones[i][5], ecualizaciones[i][6]);
+    }
+
+    printf("O pulse E para añadir un parametro de reverberacion y aplicarlo\n");
+
+    opcion = teclado() - '0';
+    while ((opcion<= 0 || opcion > parametros)&&opcion!= 21){
+      printf("Por favor pulse una tecla correcta\n");
+      opcion = teclado();
+    }
+
+    if(opcion >0 && opcion <= parametros){
+      nv = ecualizaciones [opcion - 1];
+
+      estado = 2;
+    }
+
+    else if(opcion == 21 && parametros < 9){
+      //TODO crear nuevo nv de ecualizacion
+      ecualizaciones[parametros] = //creado
+      nv = ecualizaciones[parametros];
+
+      parametros ++;
+    }
   }
